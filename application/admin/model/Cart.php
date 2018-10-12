835d463993e974b4d9b811adf0a94653332706dd
meetmr <83466349@qq.com>
2018-10-12 12:18:40 +0800

    完成后台登陆

--- application/admin/model/Cart.php
+++ application/admin/model/Cart.php
@@ -19,6 +19,7 @@ class Cart extends Model
         // 获取当前用户的购物车
         $user_id = session('user.id');
         $userCart = self::where(['u_id'=>$user_id])->select();
+
         foreach ($userCart as $item){
             $item['goods'] = Goods::getGoodsInfo($item['g_id']);
         }
